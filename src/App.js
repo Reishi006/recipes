03dbcd94ca691ed021120308524965f9e6bd5ab8
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2023-12-28 08:32:35 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="main-container">
+        <h1>Content!</h1>
+        <p>Hello World!!!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
